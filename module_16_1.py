2c4df633a8c4cb620d3af908cd6f54fb14717fb0
LadyJaine <fullsucsess@gmail.com>
2024-10-28 22:19:55 +0500

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -5,10 +5,16 @@ app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 async def main_page():
     return "Главная страница"
 
+@app.get('/user')
+async def get_user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
+
 @app.get('/user/admin')
 async def get_admin_page():
  return 'Вы вошли как администратор'
 # http://127.0.0.1:8000/user/100
+
 
 @app.get('/user/{user_id}')
 async def get_user_number(user_id: int):
